45bb1d405d0c7e567e7f867121fb7c60e5df5ade
panabobomix <panabobomix@gmail.com>
2020-04-21 18:10:08 +0200

    Initiial commit
    Dodanie funkcjonalności wyświetlenia aktualnego statusu produkcyjnego do wczesniej już utworzonego main window oraz current_production.

---
+++ current_production.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Panel_Lidera_Linii
+{
+    /// <summary>
+    /// Logika interakcji dla klasy current_production.xaml
+    /// </summary>
+    public partial class current_production : Window
+    {
+        public current_production()
+        {
+            InitializeComponent();
+        }
+    }
+}
