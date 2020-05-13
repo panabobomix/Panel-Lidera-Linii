3bd14ddcd416a36bde4461808521a99c5dd1606f
panabobomix <panabobomix@gmail.com>
2020-05-13 14:10:09 +0200

    identyfikacja pracowników
    Wyświetlanie zalogowanych operatorów po imieniu i nazwisku zamiast numeru osobowego ( złączenie dwóch baz)

--- zalogowani_operatorzy.xaml.cs
+++ zalogowani_operatorzy.xaml.cs
@@ -41,120 +41,153 @@ namespace Panel_Lidera_Linii
 
         private void downlad_all(object sender, RoutedEventArgs e)
         {
-
-            
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421052' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T01.ItemsSource = dt.DefaultView;
-                }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421975' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T02.ItemsSource = dt.DefaultView;
-                }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421022' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T03.ItemsSource = dt.DefaultView;
-                }
-
-            }
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '4211791' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T05.ItemsSource = dt.DefaultView;
-                }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '422261' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T04.ItemsSource = dt.DefaultView;
-                }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421998' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
-                {
-                    DataTable dt = new DataTable();
-                    a.Fill(dt);
-                    T06.ItemsSource = dt.DefaultView;
-                }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421050' Order by CreationDate DESC";
+                string sqlQuery = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421050'and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
                 using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
                 {
                     DataTable dt = new DataTable();
                     a.Fill(dt);
                     C01.ItemsSource = dt.DefaultView;
                 }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421913' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
+                string sqlQuery2 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421913' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery2, con))
                 {
                     DataTable dt = new DataTable();
                     a.Fill(dt);
                     C02.ItemsSource = dt.DefaultView;
                 }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '444444' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
+                string sqlQuery3 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '444444' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery3, con))
                 {
                     DataTable dt = new DataTable();
                     a.Fill(dt);
                     C03.ItemsSource = dt.DefaultView;
                 }
-            }
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();//otwarcie połączenie
-                string sqlQuery = "select TOP 1*  from tblHeaderData Where InventoryNo = '421008' Order by CreationDate DESC";
-                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery, con))
+                string sqlQuery4 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421008' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery4, con))
                 {
                     DataTable dt = new DataTable();
                     a.Fill(dt);
                     C04.ItemsSource = dt.DefaultView;
                 }
+                string sqlQuery5 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421051' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery5, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T01_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery6 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421052' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery6, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T01_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery7 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421011' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery7, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T02_031.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery8 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421976' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery8, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T02_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery9 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421978' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery9, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T02_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery10 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421977' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery10, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T02_062.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery11 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421975' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery11, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T02_072.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery12 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421910' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery12, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T03_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery13 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421022' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery13, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T03_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery14 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '422263' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery14, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T04_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery15 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '422261' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery15, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T04_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery16 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '4211792' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery16, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T05_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery17 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '4211791' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery17, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T05_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery18 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421997' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery18, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T06_032.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery19 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421998' and CreationDate >= dateadd(HOUR, datediff(HOUR, 0, GETDATE()), 0) order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery19, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    T06_042.ItemsSource = dt.DefaultView;
+                }
+                string sqlQuery20 = "select TOP 1 Worker,LastName,FirstName from [turboPCSProduction].[dbo].[tblHeaderData] as t1 inner join [turboPCSTools].[dbo].[tblRigManPerson] as t2 on t1.Worker = t2.PersonalNo where InventoryNo = '421914' order by CreationDate DESC";
+                using (SqlDataAdapter a = new SqlDataAdapter(sqlQuery20, con))
+                {
+                    DataTable dt = new DataTable();
+                    a.Fill(dt);
+                    B02.ItemsSource = dt.DefaultView;
+                }
+                con.Close();
             }
-        }
-    }
+
+
+        }        }   
+    
 }
